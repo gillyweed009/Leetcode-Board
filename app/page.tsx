cc2864c6b97937fa8543cd23174aa249d2db88df
gillyweed009 <adityajr0114@gmail.com>
2024-05-08 05:16:49 +0530

    changed border

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ModeToggle } from "@/components/ui/mode-toggle";
 export default function Home() {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-      <div className="relative px-6 pt-10 pb-8 shadow-xl ring-2 ring-white ring-opacity-200 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+      <div className="relative px-6 pt-10 pb-8 shadow-xl ring-2 ring-grey ring-opacity-10 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="flex space-x-4">
           <Button> Hello </Button>
           <ModeToggle />
