945268957acfd176da73c6765e3d847bbd9eceb9
gillyweed009 <adityajr0114@gmail.com>
2024-05-12 07:43:51 +0530

    add site footer component

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "shadcn/ui",
+  url: "https://ui.shadcn.com",
+  ogImage: "https://ui.shadcn.com/og.jpg",
+  description: "Whiteboard",
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn-ui/ui",
+  },
+}
+
+export type SiteConfig = typeof siteConfig
