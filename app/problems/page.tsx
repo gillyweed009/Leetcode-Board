422956507c95cf6f0017e663c739d94d3d656d83
gillyweed009 <adityajr0114@gmail.com>
2024-06-01 21:41:26 +0530

    added problems page

---
+++ app/problems/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle"
+
+export default function Problem() {
+  return (
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
+      <div className="relative px-6 pt-10 pb-8 shadow-xl ring-2 ring-grey ring-opacity-10 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+        <div className="flex space-x-4">A form will be added here.</div>
+      </div>
+    </div>
+  )
+}
