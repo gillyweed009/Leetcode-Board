048dbdd3eb74502c8ce71f1342cba74dd0c3b5c8
gillyweed009 <adityajr0114@gmail.com>
2024-06-02 10:38:13 +0530

    added leetcode form boiler plate code

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -1,8 +1,12 @@
+import { LeetcodeForms } from "@/components/custom/lc-forms"
+
 export default function Problem() {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
       <div className="relative px-6 pt-10 pb-8 shadow-xl ring-2 ring-grey ring-opacity-10 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-        <div className="flex space-x-4">A form will be added here.</div>
+        <div className="flex space-x-4">
+          <LeetcodeForms />
+        </div>
       </div>
     </div>
   )
