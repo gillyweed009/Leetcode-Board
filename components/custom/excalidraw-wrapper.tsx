a4c0ad9c0d801845ac98c64f3b7a08ce2eb9b373
gillyweed009 <adityajr0114@gmail.com>
2024-06-20 17:40:50 +0530

    added customisations

--- components/custom/excalidraw-wrapper.tsx
+++ components/custom/excalidraw-wrapper.tsx
@@ -1,12 +1,28 @@
 "use client";
-import { Excalidraw, convertToExcalidrawElements } from "@excalidraw/excalidraw";
+import { Excalidraw, convertToExcalidrawElements, WelcomeScreen, MainMenu } from "@excalidraw/excalidraw"
 
 //import "@excalidraw/excalidraw/index.css";
 
 const ExcalidrawWrapper: React.FC = () => {
   return (
     <div className="h-screen">
-      <Excalidraw />
+      <Excalidraw >
+        <WelcomeScreen/>
+         <WelcomeScreen.Hints.HelpHint/>.
+         <WelcomeScreen.Hints.MenuHint/>.
+         <WelcomeScreen.Hints.ToolbarHint/>.
+
+      <MainMenu>
+        <MainMenu.DefaultItems.LoadScene/>
+        <MainMenu.DefaultItems.ClearCanvas/>
+        <MainMenu.DefaultItems.ToggleTheme/>
+        <MainMenu.DefaultItems.Export/>
+        <MainMenu.DefaultItems.SaveToActiveFile/>
+        <MainMenu.DefaultItems.SaveAsImage/>
+        <MainMenu.DefaultItems.Help/>
+        <MainMenu.DefaultItems.ChangeCanvasBackground/>
+      </MainMenu>
+      </Excalidraw >
     </div>
   );
 };
